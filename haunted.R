0272a7b6d355c9eb05829554300768307591512f
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-11-01 13:53:59 -0600

    Practice Data Imputation

---
+++ haunted.R
@@ -0,0 +1,34 @@
+library(tidymodels)
+library(vroom)
+library(tidyverse)
+
+
+# Data Gathering ----------------------------------------------------------
+
+test <- vroom("test.csv")
+train <- vroom("train.csv")
+
+# train$color <- as.factor(train$color)
+# train$type <- as.factor(train$type)
+
+# # EDA ---------------------------------------------------------------------
+# ggplot(data=train, aes(x=type, y=bone_length)) +
+#   geom_boxplot()
+
+
+# Data Imputation ----------------------------------------------------------------
+missing <- vroom("trainWithMissingValues.csv")
+
+imputeRecipe <- recipe(type ~ . , data=missing) %>% 
+  update_role(id,new_role="ID") %>% 
+  step_string2factor(all_nominal_predictors()) %>% 
+  step_impute_knn(hair_length, impute_with = c('has_soul','color'), neighbors=5 ) %>% 
+  step_impute_knn(rotting_flesh, impute_with = c('has_soul','color','hair_length'), neighbors=5 ) %>% 
+  step_impute_knn(bone_length, impute_with = c('has_soul','color','hair_length','rotting_flesh'),neighbors=5)
+
+  
+preppedImpute <- prep(imputeRecipe)
+bakedImpute <- bake(preppedImpute, new_data = missing)
+
+
+rmse_vec(train[is.na(missing)],bakedImpute[is.na(missing)])
